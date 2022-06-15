dc31d8033f87b33e6ae04465fec9660940e4d62b
rivalcoba <ivan.rivalcoba@gmail.com>
2022-06-15 14:15:51 -0500

    S01 nlazado con checkboxes

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { ref } from 'vue';
+import { ref } from "vue";
 
-const header = ref('🛒 Shopping List App');
+const header = ref("🛒 Shopping List App");
 const items = ref([
-  { id: 1, label: '10 bolillos' },
-  { id: 2, label: '1 lata de frijoles' },
-  { id: 3, label: '2 lata de atún' },
+  { id: 1, label: "10 bolillos" },
+  { id: 2, label: "1 lata de frijoles" },
+  { id: 3, label: "2 lata de atún" },
 ]);
-const newItem = ref('');
+const newItem = ref("");
+const newItemHighPriority = ref(false);
 </script>
 
 <template>
   <h1>{{ header }}</h1>
   <input v-model.trim="newItem" type="text" placeholder="Add Item" />
-  {{ newItem }}
+  <!-- Caja de seleccion de prioridad -->
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority">
+    High Priority
+  </label>
+  <p>{{ newItemHighPriority }}</p>
+  <!-- Lista -->
   <ul>
     <li v-for="({ id, label }, index) in items" key="id">⚜ {{ label }}</li>
   </ul>
